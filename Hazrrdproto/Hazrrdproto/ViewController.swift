4977065d306881b9fb6a5a5d51381dcd7296a46c
Aldo Lugo <aldo@aldomatic.com>
2016-01-13 10:36:22 -0600

    Initial Commit

---
+++ Hazrrdproto/Hazrrdproto/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Hazrrdproto
+//
+//  Created by Aldo Lugo on 1/13/16.
+//  Copyright © 2016 Aldo Lugo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
